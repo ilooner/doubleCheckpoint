68cfa61592ff0f7af55093407aa0a00940e873d1
Timothy Farkas <tim@datatorrent.com>
2015-11-12 17:20:16 -0800

    Failing on double checkpointing

--- src/main/java/com/datatorrent/dcheck/Consumer.java
+++ src/main/java/com/datatorrent/dcheck/Consumer.java
@@ -1,14 +1,32 @@
 package com.datatorrent.dcheck;
 
 import com.datatorrent.api.DefaultInputPort;
+import com.datatorrent.api.Operator.CheckpointListener;
 import com.datatorrent.common.util.BaseOperator;
+import com.google.common.collect.Sets;
+import java.util.Set;
 
-public class Consumer extends BaseOperator
+public class Consumer extends BaseOperator implements CheckpointListener
 {
+  private Set<Long> checkpointed = Sets.newHashSet();
+
   public final transient DefaultInputPort<Object> input = new DefaultInputPort<Object>() {
     @Override
     public void process(Object t)
     {
     }
   };
+
+  @Override
+  public void checkpointed(long l)
+  {
+    if (!checkpointed.add(l)) {
+      throw new RuntimeException("Checkpointed the same window twice");
+    }
+  }
+
+  @Override
+  public void committed(long l)
+  {
+  }
 }
